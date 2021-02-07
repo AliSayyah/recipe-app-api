a27c63769873a0e4c4c2843640d08dbf5f48d70f
ali <ali.sayyah2@gmail.com>
2021-02-07 18:13:02 +0330

    added filter feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -42,8 +42,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get('tags')
+        ingredient = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+
+        if ingredient:
+            ingredient_id = self._params_to_ints(ingredient)
+            queryset = queryset.filter(ingredients__id__in=ingredient_id)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
