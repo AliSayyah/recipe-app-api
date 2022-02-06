d4959687ead2c95cd3d895c6a3eee5157451f70e
ali sayyah <ali.sayyah2@gmail.com>
2022-02-06 15:45:40 +0330

    Update serializers.py

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -20,21 +20,14 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serialize a recipe"""
-    ingredients = serializers.PrimaryKeyRelatedField(many=True,
-                                                     queryset=Ingredient.objects.all())
-    tags = serializers.PrimaryKeyRelatedField(many=True,
-                                              queryset=Tag.objects.all())
+    ingredients = IngredientSerializer(many=True)
+    tags = TagSerializer(many=True)
 
     class Meta:
         model = Recipe
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link')
         read_only_fields = ('id',)
 
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """serialize a recipe detail"""
-    ingredients = IngredientSerializer(many=True, read_only=True)
-    tags = TagSerializer(many=True, read_only=True)
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
