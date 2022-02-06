46c5129b6e1d279bcce94d1aeb08713b827f2b92
ali sayyah <ali.sayyah2@gmail.com>
2022-02-06 15:47:55 +0330

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -54,6 +54,10 @@ class Tag(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.name
@@ -63,6 +67,10 @@ class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.name
@@ -76,8 +84,6 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-    ingredients = models.ManyToManyField('Ingredient')
-    tags = models.ManyToManyField('Tag')
     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
